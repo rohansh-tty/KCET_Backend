abeab7b31d6b9c3e307c3e46fab3bcd735990f55
rohansh-tty <rohanshetty.dev@gmail.com>
2024-05-25 06:05:02 +0000

    feat: added signup, email templates

--- cutoff_app/core/auth.py
+++ cutoff_app/core/auth.py
@@ -1,12 +1,18 @@
-import frappe 
-from frappe.utils import random_string, get_url, logger
+import traceback
+
 import frappe
 import frappe.oauth
 from frappe import _
 from frappe.auth import LoginManager
-from frappe.utils import cint, get_url, get_datetime
-from frappe.utils.password import check_password, passlibctx, update_password
-
+from frappe.rate_limiter import rate_limit
+from frappe.utils import cint, get_datetime, get_url, logger, random_string
+from frappe.utils.password import (
+    check_password,
+    get_password_reset_limit,
+    passlibctx,
+    update_password,
+)
+from itsdangerous import URLSafeTimedSerializer  # type: ignore
 
 
 @frappe.whitelist(allow_guest=True)
@@ -45,10 +51,8 @@ def get_token(
             )  # update Auth Table with password for corresponding email
             frappe.logger(__name__).debug("Updating Auth Table with Password")
         else:
-                frappe.logger(__name__).debug(
-                    f"Checking user password, user: {user}"
-                )
-                check_password(user, password)
+            frappe.logger(__name__).debug(f"Checking user password, user: {user}")
+            check_password(user, password)
     except frappe.AuthenticationError as e:
         frappe.logger(__name__).debug("Invalid user or passowrd")
         frappe.throw("Invalid User or Password", frappe.AuthenticationError)
@@ -67,27 +71,32 @@ def get_token(
     frappe.logger(__name__).debug("Generated bearer token...")
     if user.lower() != "administrator":
         user_details = frappe.db.get_value(
-            "Employee",
+            "User",
             {
                 "email": user,
             },
-            ["profile_pic", "name", "organization"],
             as_dict=1,
         )
         frappe.logger(__name__).debug(f"user details is {user_details}")
         if user_details is not None:
-            frappe.local.response["profile_pic"] = user_details["profile_pic"]
-            frappe.local.response["user_id"] = user_details["name"]
-            frappe.local.response["organization"] = user_details["organization"]
-            org_doc = frappe.get_doc('Organization', user_details['organization'])
+            pass
+            # frappe.local.response["profile_pic"] = user_details["profile_pic"]
+            # frappe.local.response["user_id"] = user_details["name"]
+            # frappe.local.response["organization"] = user_details["organization"]
+            # org_doc = frappe.get_doc("Organization", user_details["organization"])
             # try reading it from Billing Doctype
-            frappe.local.response["pricing_plan"] = org_doc.pricing_plan if org_doc.pricing_plan else None
-            frappe.local.response["organization_type"] = org_doc.organization_type if org_doc.organization_type else ""
+            # frappe.local.response["pricing_plan"] = (
+            #     org_doc.pricing_plan if org_doc.pricing_plan else None
+            # )
+            # frappe.local.response["organization_type"] = (
+            #     org_doc.organization_type if org_doc.organization_type else ""
+            # )
 
     frappe.local.response["token"] = token["access_token"]
-    
+
     frappe.local.response.update(token)
-    
+
+
 def get_oauth_client():
     client = frappe.db.get_value("OAuth Client", {})
 
@@ -111,11 +120,13 @@ def get_oauth_client():
 
     return client
 
+
 def get_bearer_token(user, expires_in=3600):
     import hashlib
-    import jwt
+
     import frappe.auth
-    from oauthlib.oauth2.rfc6749.tokens import random_token_generator, OAuth2Token
+    import jwt
+    from oauthlib.oauth2.rfc6749.tokens import OAuth2Token, random_token_generator
 
     client = get_oauth_client()
 
@@ -166,3 +177,199 @@ def get_bearer_token(user, expires_in=3600):
     frappe.flags.jwt = id_token_encoded
 
     return token
+
+
+@frappe.whitelist(methods=["POST"], allow_guest=True)
+# @rate_limit(
+#     key="email", limit=get_password_reset_limit, seconds=24 * 60 * 60, methods=["POST"]
+# )
+def custom_signup_user(name: str = None, email: str = None, password: str = None):
+    try:
+        user_exists = frappe.db.exists("User", {"email": email})
+        print("query args >>>", frappe.request.args)
+        print("user exists >>", user_exists)
+        print("name, email, pass", name, email, password)
+        if user_exists:
+            frappe.throw(f"User with the email {email} already exists, try logging in!")
+            return {"type": "failed", "message": f"User with {email} already exists"}
+        # generate verification token and send email
+        account_verification_token = generate_email_verification_token(
+            name, email, password
+        )
+        site_config = frappe.get_site_config()
+        verification_link = (
+            site_config["frontend_baseurl"]
+            + "/verify?token="
+            + account_verification_token
+            + "&invite="
+            + str(0)
+            # invite param, to switch between signup and invite
+        )
+        email_template = frappe.render_template(
+            "cutoff_app/templates/emails/email_verify.html",
+            {
+                "name": name,
+                "link": verification_link,
+                # "task": self,
+                # "task_link": f"{frontend_baseurl}/project/{construction_project.name}/task-manager-v2/task/{self.name}",
+                # "assigned_to": frappe.utils.comma_and(sent_emails_to_names),
+            },
+        )
+        frappe.sendmail(
+            recipients=[email],
+            sender="cutoffkcet@gmail.com",
+            reference_doctype="User",
+            reference_name=name,
+            subject="Account Verification",
+            message=email_template,
+            now=True,
+        )
+        return {"type": "success", "message": "Signup successful"}
+    except Exception as e:
+        frappe.logger(__name__).error(f"User Signup failed, {traceback.print_exc(e)})")
+        frappe.throw("User Signup failed, ", traceback.print_exc(e))
+
+
+# Signup Email Verification Handler
+def generate_email_verification_token(name, email, password):
+    try:
+        secret_key = frappe.get_site_config().get("secret_key")
+        salt_value = frappe.get_site_config().get("security_salt")
+        if secret_key is None and salt_value is None:
+            frappe.logger(__name__).error("Secret Key & Salt not found in Site Config")
+            frappe.throw("Secret Key & Salt not found in Site Config")
+        serializer = URLSafeTimedSerializer(secret_key)
+        # using email, password & org in signature, to deserialize while creating employee(which happens post email verification)
+        # TODO: Password should not have any symbols, only alphanumeric OR Change this logic/delimiter
+        signature = name + ":" + email + ":" + password
+        return serializer.dumps(signature, salt=salt_value)
+    except Exception as e:
+        frappe.logger(__name__).error(f"Email Token Generation failed, {e}")
+        frappe.throw(f"Email Token Generation failed, {e}")
+
+
+def confirm_email_verification_token(token, expiration=600, verification_type="signup"):
+    from itsdangerous.exc import BadTimeSignature  # type: ignore
+
+    secret_key = frappe.get_site_config().get("secret_key")
+    salt_value = frappe.get_site_config().get("security_salt")
+    if secret_key is None and salt_value is None:
+        frappe.logger(__name__).error("Secret Key & Salt not found in Site Config")
+        frappe.throw("Secret Key & Salt not found in Site Config")
+    serializer = URLSafeTimedSerializer(secret_key)
+    try:
+        if verification_type == "signup":
+            signature = serializer.loads(token, salt=salt_value, max_age=expiration)
+            name, email, password = signature.split(":")
+            # INFO: itsdangerous handles token expiry, returns SignatureExpired Error
+            return {"name": name, "email": email, "password": password}
+        elif verification_type == "invite":
+            signature = serializer.loads(token, salt=salt_value, max_age=expiration)
+            name, email, password = signature.split(":")
+            # INFO: itsdangerous handles token expiry, returns SignatureExpired Error
+            return {
+                "name": name,
+                "email": email,
+                "password": password,
+            }
+
+        # TODO: Add a page to generate verification token, if failed the first time
+    except BadTimeSignature as e:
+        frappe.logger(__name__).error(
+            f"Failed to confirm verification token, payload: {e.payload}, date_signed: {e.date_signed}"
+        )
+        frappe.throw("Failed to confirm verification token, BadTimeSignature")
+
+    except Exception as e:
+        frappe.logger(__name__).error(
+            f"Failed to confirm verification token, {type(e).__name__}"
+        )
+        frappe.throw(f"Failed to confirm verification token, {type(e).__name__}")
+
+
+@frappe.whitelist(methods=["POST"], allow_guest=True)
+@rate_limit(
+    key="token", limit=get_password_reset_limit, seconds=24 * 60 * 60, methods=["POST"]
+)
+def email_verification_handler(token):
+    try:
+        user_info = confirm_email_verification_token(token)
+        (
+            name,
+            email,
+            password,
+        ) = (
+            user_info["name"],
+            user_info["email"],
+            user_info["password"],
+        )
+
+        frappe.logger(__name__).debug(f"email:{email}, password:{password}")
+        post_email_verification(name, email, password)
+        # Get the employee doc and give him/her a role and enable his/her employee account
+        # user_doc = frappe.get_doc(
+        #     {"doctype": "Employee", "email": user_info["email"]}
+        # )
+        # user_doc.db_set("account_enabled", 1)
+
+        return {
+            "status": "success"
+        }  # TODO: Have a standard response format for all observance whitelisted methods
+    except Exception as e:
+        frappe.logger(__name__).error(f"Email Verification failed because {e}")
+        frappe.throw(f"Email Verification failed. {e}")
+    # TODO: Add a check to see if employee with email is present, if yes, ask him to login
+    # else, enable the employee account
+
+
+def post_email_verification(name, email, password):
+    user_exists = frappe.db.exists("User", {"user_id": email})
+    frappe.logger(__name__).info(f"Employee Exists?:{user_exists}")
+    # TODO: role arg is not used as of now. By default, setting default roles for new employee
+    print("user emaial", name, email)
+    try:
+        if not user_exists:
+            user_doc = frappe.new_doc("User")
+            user_doc.update(
+                {
+                    "first_name": name,
+                    "last_name": " ",
+                    "name": name,
+                    "email": email,
+                    "enabled": 1,
+                    "send_welcome_email": True,
+                }
+            )
+            user_doc.insert(ignore_permissions=True)
+            frappe.logger(__name__).info(f"New User created:  {user_doc.as_dict()}")
+            user_doc.add_roles("Website User")
+            frappe.db.commit()
+        else:
+            frappe.throw(f"User with the email {email} already exists, try logging in!")
+
+        # notify sales team on new signup
+        # email_template = frappe.render_template(
+        #         "observance_app/templates/emails/new_signup.html",
+        #         {
+        #             "name": name,
+        #             "email": email,
+        #         },
+        #     )
+        # frappe.sendmail(
+        #     recipients=["sales@the-inkers.com"],
+        #     sender="support@the-inkers.com",
+        #     reference_doctype="Employee",
+        #     reference_name=name,
+        #     subject=f"New User Sign-Up - ${name}",
+        #     message=email_template,
+        #     now=True
+        # )
+        # create a new frappe bearer token on signup and return the same
+        get_token(email, password, new_user=True)
+
+    except Exception as e:
+        frappe.logger(__name__).error(
+            f"Exception while post_email_verification, {e}, {traceback.format_exc()}"
+        )
+        traceback.format_exc()
+        frappe.throw(traceback.format_exc())
