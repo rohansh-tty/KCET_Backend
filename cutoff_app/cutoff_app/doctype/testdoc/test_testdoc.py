fc891b4f126914d97783e894ea7172a5339d142a
rohansh-tty <rohanshetty.dev@gmail.com>
2024-06-30 11:20:39 +0000

    chore: removed testdoc

--- cutoff_app/cutoff_app/doctype/testdoc/test_testdoc.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Rohan Shetty  and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTestDoc(FrappeTestCase):
-	pass
